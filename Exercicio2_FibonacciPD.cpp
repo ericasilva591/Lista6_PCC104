88274dbbf52a1472fe81145a618e0a5c1511cbc6
Erica Silva Pinto <81305764+ericasilva591@users.noreply.github.com>
2021-06-25 17:24:18 -0300

    Update Exercicio2_FibonacciPD.cpp

--- Exercicio2_FibonacciPD.cpp
+++ Exercicio2_FibonacciPD.cpp
@@ -4,43 +4,15 @@
 #include <iostream>
 #include <vector>
 
-template <typename T>
-void print_vector(std::vector<T>& v) {//função para imprimir um vetor
-    for (auto i = v.begin(); i != v.end(); i++) {
-        std::cout << *i << " ";
-    }
-    std::cout << std::endl;
-}
-
-
-int FibonacciNum(int n, std::vector<int>& f) {
-        
-    f.push_back(1);
-    f.push_back(1);
-    
-    for (int i = 2; i < n; i++) {
-        f.push_back(0);
-        f[i] = f[i - 1] + f[i - 2];
-    }
-
-    return f[n - 1];
-
-}//Operação Base: Soma
-//C(n) = soma(i=2 to i=n-1)1 = n-1 - 2 +1 = n-2 --> O(n)
-
-int FibonacciMemory(int n) { //economia de memória, armazana somente os dois ultimo números
-
-    int num1 = 1;
+int Fibonacci_PD_BottomUp_var(int n) { //economia de memória, armazana somente os dois ultimo números - abordagem Bottom_UP
+    int num1 = 0;
     int num2 = 1;
     int numaux = 0;
 
-
-    for (int i = 0; i < n - 2; i++) {
-        
+    for (int i = 0; i < n - 1; i++) {
         numaux = num2;
         num2 = num2 + num1;
         num1 = numaux;
-
     }
 
     return num2;
@@ -48,19 +20,61 @@ int FibonacciMemory(int n) { //economia de memória, armazana somente os dois ul
 }//Operação Base: Soma
 //C(n) = soma(i=2 to i=n-1)1 = n-1 - 2 +1 = n-2 --> O(n)
 
+int Fibonacci_PD_BottomUp_vector(int n) {
+
+    std::vector<int> cache(n + 1, -1);
+    for (int i = 0; i < cache.size(); i++) {
+        if (i <= 1) {
+            cache[i] = i;
+        }
+        else {
+            cache[i] = cache[i - 1] + cache[i - 2];
+        }
+    }
+    return cache[n];
+}//Operação Base: comparação
+//C(n) = soma(i=0 to i=n)1 = n - 0 +1 = n+1 --> O(n)
+
+
+int Fibonacci_PD_TopDown(int n, std::vector<int>& cache) { //armazena na memória (vector) o fibonacci(n) - Abordagem Top_Down
+    if (cache[n] != -1) {// se ele já foi calculado, já retorna a a posição do vetor e recupera o valor armazenado 
+        return cache[n];
+    }
+    else{
+        if (n <= 1) {
+            cache[n] = n;
+        }
+        else {
+            cache[n] = Fibonacci_PD_TopDown(n - 1, cache) + Fibonacci_PD_TopDown(n - 2, cache);
+        }
+
+        return cache[n];
+    }
+}//Operação Base: soma
+//C(n) = C(n-1) + C(n-2) + 1
+
+int Fibonacci_PD_TopDown(int n) {
+    std::vector<int> cache(n + 1, -1);
+    return Fibonacci_PD_TopDown(n, cache);
+}
+
 int main()
 {
     int f = 11;
-    std::vector<int> v;
-
-    std::cout << FibonacciNum(f, v);
-    std::cout << std::endl;
-
-    std::cout << FibonacciMemory(f);
+    
+   std::cout << "Fibonacci de " << f << " Programacao dinamica BottomUP usando var:" << Fibonacci_PD_BottomUp_var(f);
 
     std::cout << std::endl;
 
-    print_vector(v);
+    std::cout << "Fibonacci de " << f << " Programacao dinamica BottomUP usando vector:" << Fibonacci_PD_BottomUp_vector(f);
+
+    std::cout << std::endl;
+
+    std::cout << "Fibonacci de " << f << " Programacao dinamica TopDown:" << Fibonacci_PD_TopDown(f);
+
+    std::cout << std::endl;
+
+   
 }
 
 // Executar programa: Ctrl + F5 ou Menu Depurar > Iniciar Sem Depuração
